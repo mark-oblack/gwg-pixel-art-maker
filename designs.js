aacc7bbb0fd0a4222b4062f3620b58a246b1b34a
mark-oblack <moblack42@gmail.com>
2018-03-26 18:22:45 -0400

    started writing makeGrid function

--- designs.js
+++ designs.js
@@ -4,7 +4,7 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
+	var gridHeight = document.getElementById("inputHeight").value;
+	var gridWidth = document.getElementById("inputWidth").value;
 
 }
