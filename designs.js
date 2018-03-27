3971d055b137d45ed5b1c302e081d86aa6ece71a
mark-oblack <moblack42@gmail.com>
2018-03-27 18:29:38 -0400

    js creates grid based on user input

--- designs.js
+++ designs.js
@@ -4,7 +4,12 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-	var gridHeight = document.getElementById("inputHeight").value;
-	var gridWidth = document.getElementById("inputWidth").value;
-
+	var height = document.getElementById("inputHeight").value;
+	var width = document.getElementById("inputWidth").value;
+	for(var i = 0; i < height; i++) {
+		$("#pixelCanvas").append("<tr class='table-row'></tr>");
+	}
+	for(var j = 0; j < width; j++) {
+		$(".table-row").append("<td></td>");
+	}
 }
