7bd57c820babf25164eedd1a8f5e6db90dd96003
mark-oblack <moblack42@gmail.com>
2018-03-28 13:30:26 -0400

    TD color fill- inProgress
    Started adding script to change background color of tr, td elements on hover

--- designs.js
+++ designs.js
@@ -3,12 +3,23 @@
 
 // When size is submitted by the user, call makeGrid()
 
+$(document).ready(function() {
+	$("tr, td").hover(function() {
+		$(this).css("background-color", "black");
+	});
+});
+
 function makeGrid() {
+	//clears current grid each time user hits submit
+	$("#pixelCanvas").html('');
+	//sets height and width based on user input
 	var height = document.getElementById("inputHeight").value;
 	var width = document.getElementById("inputWidth").value;
+	//adds number of rows equal to given height
 	for(var i = 0; i < height; i++) {
 		$("#pixelCanvas").append("<tr class='table-row'></tr>");
 	}
+	//adds table data element to each row based on given width
 	for(var j = 0; j < width; j++) {
 		$(".table-row").append("<td></td>");
 	}
